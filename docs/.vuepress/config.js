f27fd80f8fb4c5338cdae5bbb2fea882f3cd6361
Dxxxxxxww <gotu2018@163.com>
2019-12-31 15:48:40 +0800

    新建个人博客 by dxw

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  base: '/blog/',
+  title: 'Dxx',
+  description: '我的个人网站',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/Dxxxxxxww' },
+      { text: 'External', link: 'https://google.com' }
+    ],
+    sidebar: [
+      {
+        title: '前端小事',
+        children: [
+          ['/front-end/', 'heihei'],
+          ['/front-end/utils/', 'JS工具函数集合']
+        ]
+      },
+      {
+        title: 'CSS点滴',
+        children: [['/css/', '哈哈哈']]
+      },
+      {
+        title: '其他',
+        children: [['/others/', '哈哈哈']]
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /girl.jpg
+heroText: Dxx's Personal Blog
+tagline: 欢迎来到我的个人站点
+actionText: 这里进来 ~
+actionLink: /front-end/
+features:
+  - title: 前段小事
+    details: 包括但不限于 js, es6, vue, react 等平时记录的细节
+  - title: CSS点滴
+    details: 虽然叫着'css点滴'，但其实是包含 echarts three 等可视化的东西
+  - title: 其他
+    details: 主要是用来存放 flutter, py 等兴趣相关的资料
+footer: MIT Licensed | Copyright © 2019-present Dxx
+---
