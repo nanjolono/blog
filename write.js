de304ca1dad326e6db11fc7e2b617622de9644d2
dxw <gotu2018@163.com>
2020-11-26 07:51:26 +0800

    fix：提交工具函数之一格式化金额（','分割的显示） by dxw

--- write.js
+++ write.js
@@ -1,51 +1,16 @@
-function a(str) {
-  const list = str.split('')
-  const indexList = []
-  list.forEach((v, i) => {
-    if (!(i % 3)) {
-      indexList.push(i)
-    }
-  })
-  indexList.sort((a, b) => b - a)
-  indexList.forEach((v) => {
-    if (v) {
-      list.splice(v, 0, ',')
-    }
-  })
-  console.log(list.join(''))
-}
+const arr = ['a', 'b', 'c']
 
-function a2(str) {
-  const index = str.indexOf('.'),
-    dot = ','
-  let pre, suf
-  if (index !== -1) {
-    pre = str.slice(0, index)
-    suf = str.slice(index)
-  } else {
-    pre = str
+let val = arr[0]
+Object.defineProperty(arr, '0', {
+  get: function() {
+    console.log(123)
+    return val
+  },
+  set: function (newVal) {
+    val = newVal + 1
   }
+})
 
-  const list = pre.split('')
-  pre = ''
-
-  let v
-  for (let i = list.length - 1; i >= 0; i--) {
-    v = list[i]
-    pre = v + pre
-    if (!(i % 3)) {
-      pre = dot + pre
-    }
-  }
-
-  if (suf) {
-    pre = pre + suf
-  }
-  if (pre.indexOf(dot) === 0) {
-    pre = pre.slice(1)
-  }
-  console.log(pre)
-  return pre
-}
-
-a2('123456789')
+console.log(arr[0])
+arr[0]  = 100
+console.log(arr[0])
