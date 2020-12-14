d6083cc985dbe8b42d00c42065059f3eaddfe518
dxw <gotu2018@163.com>
2020-12-14 16:39:28 +0800

    fix：执行上下文。 btw 最近真的是太忙了都没更新 by dxw

--- write.js
+++ write.js
@@ -1,16 +1,52 @@
-const arr = ['a', 'b', 'c']
+function filterMenuRoutes(router) {
+  debugger
+  let result = []
+  result = router.filter((r) => {
+    if (r.children && r.children.length > 0) {
+      r.children = result = filterMenuRoutes(r.children)
+      return result
+    } else {
+      return !r.meta.isFunctional
+    }
+  })
+  return result
+}
 
-let val = arr[0]
-Object.defineProperty(arr, '0', {
-  get: function() {
-    console.log(123)
-    return val
+const asyncRoutes = [
+  {
+    path: '/merchant',
+    name: 'Merchant',
+    children: [
+      {
+        path: 'infoMng',
+        name: 'InfoMng',
+        meta: {
+          isFunctional: false,
+        },
+      },
+      {
+        path: 'infoMng/add',
+        name: 'InfoMngAdd',
+        meta: {
+          isFunctional: true, // 功能模块
+        },
+      },
+    ],
+    meta: {
+      icon: '',
+      isFunctional: false,
+    },
   },
-  set: function (newVal) {
-    val = newVal + 1
-  }
-})
+  {
+    path: '/auditRecord',
+    name: 'AuditRecord',
+    meta: {
+      icon: '',
+      // roles: []
+      isFunctional: false,
+    },
+  },
+]
 
-console.log(arr[0])
-arr[0]  = 100
-console.log(arr[0])
+const abc = filterMenuRoutes(asyncRoutes)
+console.log(abc)
